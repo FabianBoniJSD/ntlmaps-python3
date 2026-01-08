7f98dedb6387ead8640783204de69cb17710395c
Fabian Boni <fabian.boni@jsd.bs.ch>
2026-01-08 12:47:55 +0100

    Fix copyright notice and improve path handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 # This file is part of 'NTLM Authorization Proxy Server'
-# Copyright 2001 Dmitry A. Rozmanov <dima@xenon.spb.ru>
+# Copyright 2001 Dmitry A. Rozmanov <dima@xenon. spb.ru>
 #
 # NTLM APS is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,24 +11,25 @@
 # NTLM APS is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more details. 
 #
 # You should have received a copy of the GNU General Public License
 # along with the sofware; see the file COPYING. If not, write to the
 # Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 #
-import __init__
 
 import sys
+import os
 
 import server, config, config_affairs
 
 
 #--------------------------------------------------------------
 # config affairs
-# look for default config name in lib/config.py
-conf = config.read_config(config.findConfigFileNameInArgv(sys.argv, __init__.ntlmaps_dir+'/'))
+# look for default config name in lib/config. py
+ntlmaps_dir = os. path.dirname(os.path. abspath(__file__))
+conf = config.read_config(config.findConfigFileNameInArgv(sys.argv, ntlmaps_dir+'/'))
 
 conf['GENERAL']['VERSION'] = '0.9.9'
 
